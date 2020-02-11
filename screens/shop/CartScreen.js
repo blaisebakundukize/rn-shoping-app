052b3d9b365f18ab4fb6677c8ab7862c5e943abe
blaisebakundukize <blaisebakundukize@gmail.com>
2020-02-11 12:53:22 +0200

    uploaded to github

---
+++ screens/shop/CartScreen.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  FlatList,
+  Button,
+  ActivityIndicator
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+
+import Colors from "../../constants/Colors";
+import CartItem from "../../components/shop/CartItem";
+import Card from "../../components/UI/Card";
+import * as cartActions from "../../store/actions/cart";
+import * as orderActions from "../../store/actions/orders";
+
+const CartScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
+  const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+  const cartItems = useSelector(state => {
+    const transFormedCartItems = [];
+    for (const key in state.cart.items) {
+      transFormedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum
+      });
+    }
+    return transFormedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1
+    );
+  });
+  const dispatch = useDispatch();
+
+  const sendOrderHandler = async () => {
+    setIsLoading(true);
+    await dispatch(orderActions.addOrder(cartItems, cartTotalAmount));
+    setIsLoading(false);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Card style={styles.summary}>
+        <Text style={styles.summaryText}>
+          Total:
+          <Text style={styles.amount}>
+            ${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}
+          </Text>
+        </Text>
+        {isLoading ? (
+          <ActivityIndicator size='small' color={Colors.primary} />
+        ) : (
+          <Button
+            color={Colors.accent}
+            title='Order Now'
+            disabled={cartItems.length === 0}
+            onPress={sendOrderHandler}
+          />
+        )}
+      </Card>
+      <FlatList
+        data={cartItems}
+        keyExtractor={item => item.productId}
+        renderItem={({ item }) => (
+          <CartItem
+            quantity={item.quantity}
+            title={item.productTitle}
+            amount={item.sum}
+            deletable
+            onRemove={() =>
+              dispatch(cartActions.removeFromCart(item.productId))
+            }
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+CartScreen.navigationOptions = {
+  headerTitle: "Your Orders"
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20
+  },
+  summary: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    padding: 10
+  },
+  summaryText: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18
+  },
+  amount: {
+    color: Colors.primary
+  }
+});
+
+export default CartScreen;
